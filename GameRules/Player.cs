3592288ad75102eee31728e45c67375ecc822440
Diego-Hiriart <hiriart.leon.d@gmail.com>
2021-12-18 16:54:20 -0500

    Full rules

---
+++ GameRules/Player.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameRules
+{
+    public class Player
+    {
+        private float health;
+        private int lives;
+        private int pickUpCount;
+        private float speed;
+
+        public float Health { get { return this.health; } }
+        public int Lives { get { return this.lives; } }
+        public int PickUpCount { get { return this.pickUpCount; } }
+        public float Speed { get { return this.speed; } }
+
+        public Player() {
+            this.health = 100;
+            this.lives = 3;
+            this.pickUpCount = 0;
+            this.speed = 4f;
+        }
+
+        public Player(float health, int lives, int pickUps, float speed)
+        {
+            this.health = health;
+            this.lives = lives;
+            this.pickUpCount = pickUps;
+            this.speed = speed;
+        }
+
+        public void SetHealth(float value)
+        {
+            this.health = value;
+        }
+
+        public void SetLives(int number)
+        {
+            this.lives = number;
+        }
+
+        public void SetPickUpCount(int count)
+        {
+            this.pickUpCount = count;
+        }
+
+        public void SetSpeed(float value)
+        {
+            this.speed = value;
+        }
+
+        public float GetHealth()
+        {
+            return this.health;
+        }
+
+        public int GetLives()
+        {
+            return this.lives;
+        }
+
+        public int GetPickUpCount()
+        {
+            return this.pickUpCount;
+        }
+
+        public float GetSpeed()
+        {
+            return this.speed;
+        }
+    }
+}
--- GameRules/Program.cs
+++ GameRules/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicroRuleEngine;
+using System.Linq.Expressions;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace GameRules
 {
@@ -10,7 +14,51 @@ namespace GameRules
     {
         static void Main(string[] args)
         {
+            MRE engine = new MRE();
 
+            Rule health = new Rule//If health is less than 15 (very low)
+            {
+                MemberName = "Health",
+                Operator = ExpressionType.LessThanOrEqual.ToString("g"),
+                TargetValue = 15
+            };
+            Func<Player, bool> healthCompiled = engine.CompileRule<Player>(health);
+
+            Rule speedUp = Rule.Create("PickUpCount", mreOperator.GreaterThanOrEqual, 4) &
+                Rule.Create("Speed", mreOperator.LessThanOrEqual, 10);//Pick ups give more speed if speed isnt to high already and you have som pick ups
+            Func<Player, bool> speedUpCompiled = engine.CompileRule<Player>(speedUp);
+
+            Rule pickUpsHeal = Rule.Create("PickUpCount", mreOperator.GreaterThan, 6) &
+                Rule.Create("Lives", mreOperator.LessThanOrEqual, 2) &
+                Rule.Create("Health", mreOperator.LessThan, 30);//Health if lives and health are low
+            Func<Player, bool> pickUpsHealCompiled = engine.CompileRule<Player>(pickUpsHeal);
+
+            List<Rule> rulesList = new List<Rule>()
+            {
+                health,
+                speedUp,
+                pickUpsHeal
+            };
+
+            Player player = new Player();
+
+            Console.WriteLine(healthCompiled(player));
+            Console.WriteLine(speedUpCompiled(player));
+            Console.WriteLine(pickUpsHealCompiled(player));            
+
+            player.SetHealth(20);
+            player.SetPickUpCount(7);
+            player.SetSpeed(9);
+            player.SetLives(2);
+
+            Console.WriteLine(healthCompiled(player));
+            Console.WriteLine(speedUpCompiled(player));
+            Console.WriteLine(pickUpsHealCompiled(player));
+
+            string rulesJson = JsonConvert.SerializeObject(rulesList, Formatting.Indented);//Indented for nicer format and easier reading
+            File.WriteAllText(@"..\..\..\Assets\Scripts\PlayerRules.json", rulesJson);
+
+            Console.ReadLine();
         }
     }
 }
